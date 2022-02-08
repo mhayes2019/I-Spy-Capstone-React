f23ac8388c8b4f8655492b5499ec33745c53110f
Melinda <melindahayes84@gmail.com>
2022-02-08 11:04:31 -0800

    connected login to gmail account

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,17 @@ import React from "react"
 import {Link} from "react-router-dom"
 import "./Home.css"
 export const Home = ()=> {
+
+    const currentUserName = localStorage.getItem('currentUserName');
+    const currentUserEmail = localStorage.getItem('currentUserEmail');
+
+
+    console.log({ currentUserName, currentUserEmail, })
     return(
         <div>
         <h1>I Spy</h1>
+        <h2>Welcome {currentUserName}</h2>
+        <h3>{currentUserEmail}</h3>
     <div className="home-wrapper">
         <Link className="home-button" to="/challenge">Challenge A Friend</Link>
     </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,33 +1,89 @@
-import React from "react";
+
+import {React, useState} from "react";
 import { GoogleLogin } from 'react-google-login';
+import {Link, useNavigate} from "react-router-dom"
 
-export class LoginPage extends React.Component{
 
-    responseGoogle=(response)=>{
+//creating a component for navigation
+export const LoginPage = () => {
+
+    let navigate = useNavigate();
+    const responseGoogle = (response) => {
+
+    };
+
+    const successLogin = (response) => {
+        console.log("success", response.profileObj)
+
+        // you should maybe probably make an call here
+        // either if email exists in db, then good if not, add email to db
+        // for now, storing this in local storage which saves stuff in the browser
+        localStorage.setItem('currentUserName', response.profileObj.name)
+        localStorage.setItem('currentUserEmail', response.profileObj.email)
+
         
-        console.log(response);
-        console.log(response.profileObj);
-        
+        // after stored, redirect to home page
+        navigate('/');
     }
-    render() {
-        
-        console.log(this.responseGoogle)
 
-        // using fragments (<>) to be able to hold more than one div
-        return(
-            <> <div> I Spy <x-search>{this.props.name}</x-search>!</div>
-        
-            <div>
-                <GoogleLogin
-                    clientId="920359438731-fu6pqs2e5umadnp574h3kl5p95rhqd1d.apps.googleusercontent.com"
-                    buttonText="Login" 
-                    onSuccess={this.responseGoogle}
-                    onFailure={this.responseGoogle}
-                    cookiePolicy={'single_host_origin'}
+    return(
+    <div>
+        <GoogleLogin
+            clientId="920359438731-fu6pqs2e5umadnp574h3kl5p95rhqd1d.apps.googleusercontent.com"
+            buttonText="Login" 
+            onSuccess={successLogin}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+            redirectUrl={"www.cats.com"}
                     
-                    /> 
-                </div></>
-        )
+        /> 
+ </div>
+  )
+}
+
+export default LoginPage
+
+
+// import React from "react";
+// import { GoogleLogin } from 'react-google-login';
+// import {Link} from "react-router-dom"
+
+
+// export class LoginPage extends React.Component{
+//     state = {
+//         success: "",
+//     }
+//     goHome = <Link to="/">Home</Link>
+//     responseGoogle=(response)=>{
         
-    }
-} 
+//         console.log(response);
+//         console.log(response.profileObj);
+//         this.setState({ success: "success" });
+//     }
+
+//     render() {
+//         if (this.state.success === "success") {
+//             console.log("condition hit")
+
+//         }
+//         console.log("in the render", this.state.success)
+
+//         // using fragments (<>) to be able to hold more than one div
+//         return(
+//             <> <div> I Spy <x-search>{this.props.name}</x-search>!</div>
+        
+//             <div>
+//                 <GoogleLogin
+//                     clientId="920359438731-fu6pqs2e5umadnp574h3kl5p95rhqd1d.apps.googleusercontent.com"
+//                     buttonText="Login" 
+//                     onSuccess={this.responseGoogle}
+//                     onFailure={this.responseGoogle}
+//                     cookiePolicy={'single_host_origin'}
+//                     // redirectUrl={'localhost:3000/'}
+                    
+//                     /> 
+//                 </div></>
+//         )
+        
+//     }
+// } 
